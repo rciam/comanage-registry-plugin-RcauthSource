fbc3bac065ff4c556235c94425f0b44fb601e28c
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2021-03-26 16:31:12 +0200

    Fixed PHP fatal error

--- Lib/enum.php
+++ Lib/enum.php
@@ -2,18 +2,18 @@
 
 class RcauthSourceMPEndpointEnum
 {
- public const AUTH_ENDPOINT = 'authorization_endpoint';
- public const REGISTER_ENDPOINT = 'registration_endpoint';
- public const JWKS_URI = 'jwks_uri';
- public const ISSUER = 'issuer';
- public const TOKEN_ENDPOINT = 'token_endpoint';
- public const USERINFO_ENDPOINT = 'userinfo_endpoint';
- public const TOKEN_ENDPOINT_AM_SUP = 'token_endpoint_auth_methods_supported';
- public const SUBJECT_TYP_SUP = 'subject_types_supported';
- public const SCOPES_SUP = 'scopes_supported';
- public const RESPONSE_TYP_SUP = 'response_types_supported';
- public const CLAIMS_SUP = 'claims_supported';
- public const ID_TOKEN_SIG_ALG_VAL_SUP = 'id_token_signing_alg_values_supported';
+  const AUTH_ENDPOINT = 'authorization_endpoint';
+  const REGISTER_ENDPOINT = 'registration_endpoint';
+  const JWKS_URI = 'jwks_uri';
+  const ISSUER = 'issuer';
+  const TOKEN_ENDPOINT = 'token_endpoint';
+  const USERINFO_ENDPOINT = 'userinfo_endpoint';
+  const TOKEN_ENDPOINT_AM_SUP = 'token_endpoint_auth_methods_supported';
+  const SUBJECT_TYP_SUP = 'subject_types_supported';
+  const SCOPES_SUP = 'scopes_supported';
+  const RESPONSE_TYP_SUP = 'response_types_supported';
+  const CLAIMS_SUP = 'claims_supported';
+  const ID_TOKEN_SIG_ALG_VAL_SUP = 'id_token_signing_alg_values_supported';
 }
 
 class RcauthSourceAssuranceComponentEnum
